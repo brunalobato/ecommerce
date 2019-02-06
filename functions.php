cb6cd2df3f7ce475892fd9c5d4b59350efab328a
Bruna <bruna.lobatotomaz@gmail.com>
2019-02-06 19:29:07 -0200

    pedidos

--- functions.php
+++ functions.php
@@ -1,11 +1,13 @@
 <?php
 
 use \Hcode\model\User;
+use \Hcode\model\Cart;
+
 
 function formatPrice($vlprice)
 {
     if(!$vlprice > 0) $vlprice =0;
-    
+
     return number_format($vlprice, 2, ",",".");
 }
 
@@ -19,5 +21,23 @@ function getUserName()
     $user = User::getFromSession();
     return $user->getperson();
 }
+
+function getCartNrtQtd()
+{
+    $cart = Cart::getFromSession();
+
+    $totals = $cart->getProductsTotals();
+
+    return $totals['nrqtd'];
+}   
+
+function getCartVlSubTotal()
+{
+    $cart = Cart::getFromSession();
+
+    $totals = $cart->getProductsTotals();
+
+    return formatPrice($totals['vlprice']);
+}   
 
 ?>
