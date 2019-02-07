3e37760181eced262f44b73f84a2670fe856f0f0
Bruna <bruna.lobatotomaz@gmail.com>
2019-02-06 22:18:19 -0200

    admin-pedidos

--- vendor/hcodebr/php-classes/src/model/orderStatus.php
+++ vendor/hcodebr/php-classes/src/model/orderStatus.php
@@ -11,5 +11,12 @@ class orderStatus extends model {
     const AGUARDANDO_PAGAMENTO =2;
     const PAGO =3;
     const ENTREGUE =4;
+
+    public static function listAll()
+    {
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_ordersstatus ORDER BY desstatus");
+    }
 }
 ?>
